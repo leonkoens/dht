3eef17792b6c950e8a6e12baa3c7a0dcd31b7cb6
Leon Koens <info@leonkoens.nl>
2015-06-22 18:05:20 +0200

    Raise an Exception if the Bucket is full.

--- dht/bucket.py
+++ dht/bucket.py
@@ -7,21 +7,28 @@ class BucketHasSelfException(Exception):
 
 
 class NodeNotFoundException(Exception):
-    message = 'Node not found'
+    pass
 
 
 class NodeAlreadyAddedException(Exception):
     pass
 
 
+class BucketIsFullException(Exception):
+    pass
+
 
 class Bucket:
     """ A Bucket is a list of sorted Nodes by last_seen. """
 
-    def __init__(self, max_size=settings.BUCKET_SIZE):
+    def __init__(self,
+                 nodes_size=settings.BUCKET_SIZE,
+                 replacement_cache_size=settings.BUCKET_REPLACEMENT_CACHE_SIZE):
         """ Init the Bucket. """
         self.nodes = []
-        self.max_size = max_size
+        self.nodes_size = nodes_size
+        self.replacement_cache = []
+        self.replacement_cache_size = replacement_cache_size
         self.has_self = False
 
     def add_node(self, node):
@@ -37,8 +44,15 @@ class Bucket:
             self.find_node(node.key)
             raise NodeAlreadyAddedException()
         except NodeNotFoundException:
+            pass
+
+        if len(self.nodes) < self.nodes_size:
             self.nodes.append(node)
             self.sort()
+        elif len(self.replacement_cache) < self.replacement_cache_size:
+            self.add_replacement(node)
+        else:
+            raise BucketIsFullException()
 
     def find_node(self, key):
         """ Find and return a Node by key in this Bucket. """
@@ -57,3 +71,6 @@ class Bucket:
     def sort(self):
         """ Sort the nodes of this Bucket by last_seen. """
         self.nodes.sort(key=lambda node: node.last_seen)
+
+    def add_replacement(self, node):
+        self.replacement_cache.append(node)
