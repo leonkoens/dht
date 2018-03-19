3c20f62692b3f128b2e52592e1f20aec0e1174e0
Leon Koens <info@leonkoens.nl>
2018-03-19 13:09:00 +0100

    Add test for BucketTree.find_nodes()

--- tests/test_routing.py
+++ tests/test_routing.py
@@ -1,16 +1,17 @@
 import unittest
 
-from dht.node import Node, SelfNode
-from dht.routing import BucketTree
-from dht.utils import hash_string
+from node import Node, SelfNode
+from routing import BucketTree
+from utils import hash_string
 
-from dht.bucket import NodeAlreadyAddedException, BucketIsFullException
-from dht import settings
+from bucket import NodeAlreadyAddedException, BucketIsFullException
+import settings
 
 
 class BucketTreeTest(unittest.TestCase):
     """
     BucketTree.find_node(key)
+    BucketTree.find_nodes(key)
     BucketTree.add_node(node)
     """
 
@@ -58,7 +59,7 @@ class BucketTreeTest(unittest.TestCase):
         # Looking for the key again should return the same Node.
         node2 = tree.find_node(key)
 
-        self.assertEquals(node1, node2)
+        self.assertEqual(node1, node2)
 
     def test_full_bucket(self):
         """ A Bucket should raise an BucketIsFullException when the Bucket's Node list and
@@ -83,3 +84,31 @@ class BucketTreeTest(unittest.TestCase):
         # Bucket should be full by now.
         with self.assertRaises(BucketIsFullException):
             tree.add_node(node)
+
+    def test_find_nodes(self):
+
+        tree = self.get_new_tree()
+
+        for i in range(18):
+            key = hash_string(str(i))
+            node = Node(key=key)
+            tree.add_node(node)
+
+        nodes = tree.find_nodes(hash_string('test'))
+
+        # 18 other nodes + self = 19.
+        self.assertEqual(len(nodes), 19)
+
+        tree.add_node(Node(hash_string('extra')))
+        nodes = tree.find_nodes(hash_string('test'))
+
+        # 19 other nodes + self = 20.
+        self.assertEqual(len(nodes), 20)
+
+        tree.add_node(Node(hash_string('more than 20')))
+        nodes = tree.find_nodes(hash_string('test'))
+
+        # 20 other nodes + self = 20 because of the bucket size.
+        self.assertEqual(len(nodes), 20)
+
+
